33569f0695f6b055c1bd483b36f1d4192e56dbe8
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-05-21 23:04:47 +1000

    Class Extend Event Emitter
    In this commit we create class and extend its prototype from
    Event Listener and then created a method that emit. So every
    object created with get that method.

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const greet = require("./greet");
+const EventEmitter = require("events");
 
-greet.english();
-greet.spanish();
+//We inherit Event Emitter properties
+class Saludos extends EventEmitter {
+    constructor() {
+        super();
+        this.saludo = "emitMessage";
+    }
+    //We create a method that EMIT with some data
+    despedida() {
+        console.log("ADIOS");
+        this.emit("emitMessage", "Julio")
+    };
+};
+
+var newSaludo = new Saludos();
+
+// Then we add a LISTENER in our new object, created with the CLASS constractor, that listen to an EMIT and takes DATA
+newSaludo.on("emitMessage", (data) => {
+    console.log(data);
+});
+
+newSaludo.despedida();
+
+
+
+
+
