3c92dd647cfaecba300b4e3fa06ed3ab7937e8ad
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-05-24 08:48:13 +1000

    Use of createReadStream() & createWriteStream()

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const greet = require("./greet");
+const fs = require("fs");
 
-greet.english();
-greet.spanish();
+// Syncronous method will be called and the whole programm will wait for it
+var saludo2 = fs.readFileSync(__dirname + "/saludos.txt", "utf8");
+
+console.log(`This is syncro method: ${saludo2}`);
+
+// Here while waiting, the rest of the code will cointinue and then once the code is done, the function CALLBACK will be invoke;
+var saludo = fs.readFile(__dirname + "/saludos.txt", "utf8", function(err, data) {
+    console.log(data);
+});
+
+console.log("Despues de fs.readFile()")
+
+var copiame = fs.createReadStream(__dirname + "/copiame.txt", { encoding: "utf8", highWaterMark: 1 * 1024});
+
+var copiahere = fs.createWriteStream(__dirname + "/copiahere.txt");
+
+// This listener METHOD, will be trigger once the fs.createReadStream() reads the DATA, that is why the event emitter is called "data". After this we just pass a CALLBACK function to do whatever we want with the data passed, normally called CHUNK.
+copiame.on("data", (chunk) => {
+    console.log("The file has been read");
+    console.log(chunk.length);
+    // Here we are writing to the STREAM with fs.createWriteStream() method, using that chunk that qwe received from DATA event.
+    copiahere.write(chunk);
+    console.log("The file has been copied")
+});
+
+copiame.on("end", () => {
+    console.log("File read and closed");
+});
+
+
+
