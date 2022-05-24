0eb59c464af9f22a4cd047b565d109b60ae7fefa
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-05-24 10:33:26 +1000

    PIPELINE method using Stream Module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const zlib = require("zlib");
+const { pipeline } = require("stream");
 
 // A Stream that READS data from the STREAM meaning it could SEND DATA to other stream
 var readable = fs.createReadStream(__dirname + "/copiame.txt", { encoding: "utf8", highWaterMark: 1 * 1024});
@@ -13,23 +14,22 @@ var compressed = fs.createWriteStream(__dirname + "/copiame.txt.gz");
 // A zlib Object that is TRANSFORM and DUPLEX STREAM, meaning that could READ from a Stream and WRITE to another Stream.
 var gzip = zlib.createGzip();
 
-// PIPES could be invoke by READABLE Streams and send to WRITABLE Streams. It basically does the same that the .on function but RETURN the destination, in this case the Writable or Trasnform or Duplex STREAM. SO we could CHAIN more pipes
 
-// copiame.on("data", (chunk) => {
-//     // Here we are writing to the STREAM with fs.createWriteStream() method, using that chunk that qwe received from DATA event.
-//     copiahere.write(chunk);
-// });
-readable.pipe(writable);
-console.log("From readable to writable DONE")
+pipeline(readable, gzip, compressed, (err) => {
+    if (err) {
+        console.error("An error has occurred: ", err);
+        process.exitCode = 1;
+    }
+    console.log("PIPELINE from readable ---to--- gzip ---to--- compressed DONE")
+});
 
-// Compressing or decompressing a stream (such as a file) can be accomplished by piping the source stream (READABLE) through a zlib Transform (READABLE AND WRITABLE) stream into a destination stream (WRITABLE):
-readable.pipe(gzip).pipe(compressed);
-console.log("From readable ---to--- Transform gzip ---to--- Writable compressed DONE");
-
-// Examples
-// READABLE.pipe(WRITABLE or TRANSFORM or DUPLEX)
-// READABLE.pipe(TRANSFORM or DUPLEX).pipe(TRANSFORM or DUPLEX).pipe(WRITABLE)
-
+pipeline(readable, writable, (err) => {
+    if (err) {
+        console.error("An error has occurred: ", err);
+        process.exitCode = 1;
+    }
+    console.log("PIPELINE from readable ---to--- writable DONE")
+});
 
 
 
